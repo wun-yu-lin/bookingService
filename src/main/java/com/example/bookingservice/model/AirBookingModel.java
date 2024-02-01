edf0afefd4b7154380bbb7c85fb70e16030dfa86
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2024-02-01 23:32:19 +0800

    修正bookig air, booking ocean model

--- src/main/java/com/example/bookingservice/model/AirBookingModel.java
+++ src/main/java/com/example/bookingservice/model/AirBookingModel.java
@@ -8,8 +8,8 @@ import jakarta.persistence.*;
 @Table(name = "air_booking")
 @AttributeOverrides({
         @AttributeOverride(name = "id", column = @Column(name = "air_booking_id")),
-        @AttributeOverride(name = "loading", column = @Column(name = "airport_of_loading")),
-        @AttributeOverride(name = "discharge", column = @Column(name = "airport_of_discharge"))
+        @AttributeOverride(name = "loading", column = @Column(name = "airport_of_loading", length = 50, nullable = false)),
+        @AttributeOverride(name = "discharge", column = @Column(name = "airport_of_discharge", length = 50, nullable = false))
 
 })
 public class AirBookingModel extends BookingModel {
--- src/main/java/com/example/bookingservice/model/OceanBookingModel.java
+++ src/main/java/com/example/bookingservice/model/OceanBookingModel.java
@@ -9,8 +9,8 @@ import jakarta.persistence.*;
 
 @AttributeOverrides({
         @AttributeOverride(name = "id", column = @Column(name = "ocean_booking_id")),
-        @AttributeOverride(name = "loading", column = @Column(name = "port_of_loading")),
-        @AttributeOverride(name = "discharge", column = @Column(name = "port_of_discharge"))
+        @AttributeOverride(name = "loading", column = @Column(name = "port_of_loading", length = 50, nullable = false)),
+        @AttributeOverride(name = "discharge", column = @Column(name = "port_of_discharge", length = 50, nullable = false))
 })
 public class OceanBookingModel extends BookingModel {
 }
